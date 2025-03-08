6b0857cfbcb92c9392f512b117a55ddddfb63a39
Bots.Business <git.sync@bots.business>
2025-03-08 09:17:58 +0000

    Bots.Business export for bot id: 1918371

--- commands/_start.js
+++ commands/_start.js
@@ -4,12 +4,25 @@
   need_reply: false
   auto_retry_time: 
   folder: 
-  answer: 
-  keyboard: 
+
+  <<ANSWER
+
+  ANSWER
+
+  <<KEYBOARD
+
+  KEYBOARD
   aliases: 
   group: 
 CMD*/
 
 Bot.sendMessage("welcome messge") // welcome msg here
+
+
+var getRiddle = Bot.getProperty("globalRiddle")
+
+var myRiddle = {
+  }
+User.setProp()
 
 Bot.runCommand("Game")
